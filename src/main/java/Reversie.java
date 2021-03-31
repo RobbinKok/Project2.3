ffba341f3179568a5fe119c2cf0d4c134c8a0b35
JaronOun <jaronoun@hotmail.com>
2021-03-31 15:02:03 +0200

    Reversie Update

--- src/main/java/Reversie.java
+++ src/main/java/Reversie.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Reversie {
+
     private static final int BLACK = 0;
     private static final int WHITE = 1;
     private static final int EMPTY = 2;
@@ -18,7 +19,6 @@ public class Reversie {
     private int side=BLACK; 
     private int blackScore;
     private int whiteScore;
-
 
     public Reversie() {
         resetBoard();
@@ -52,14 +52,25 @@ public class Reversie {
         for (int i = 0; i < board.length; i++) {
             Arrays.fill(board[i], EMPTY);
         }
-        board[3][3] = WHITE;
-        board[3][4] = BLACK;
-        board[4][4] = WHITE;
-        board[4][3] = BLACK;
+        board[3][4] = WHITE;
+        board[3][3] = BLACK;
+        board[4][3] = WHITE;
+        board[4][4] = BLACK;
+    }
+
+    enum Directions {
+        NW(-1, -1), N(-1,0), NE(-1, 1), E(0,1), SE(1, 1), S(1,0), SW(1, -1), W(0,-1);
+
+        public final int[] dir;
+
+        private Dir(int[] dir) {
+            this.dir = dir;
+        }
     }
  
     public void playMove(int x, int y) {
         // TODO: Fill board based on player color (better implementation)
+        possibleMoves(side);
         board[x][y] = side;
         if (side==BLACK) this.side=WHITE; else this.side=BLACK;
     }
@@ -72,21 +83,51 @@ public class Reversie {
         }
     }
 
-    public void flipColor(int k, int s, int player, int x, int y) {
-        for (int c=k-1; c<=k+1; c++) {
-            for (int u=s-1; u<=s+1; u++) {
-                if (board[k][s]!=player && board[k][s]!=EMPTY) {
-                                          
-                }
+    public boolean flip(int player, int k, int s) {
+        if (board[k][s]!=player && board[k][s]!=EMPTY) {
+            board[k][s]=player;
+            return true;
+        } else {
+            if(board[k][s]!=EMPTY) {
+                board[k][s]=player;
+                return false;
             }
+        }
+        return false;
+    }
+
+    public int[] flipColor(int k, int s, int player, int x, int y) {
+        int[] dir = {k-x, s-y};
+        int[] possibleMove;
+        switch (dir) {
+            case Directions.NW.dir:
+                break;
+            case Directions.N.dir:
+                break;
+            case Directions.NE.dir:
+                break;
+            case Directions.E.dir:
+                break;
+            case Directions.SE.dir:
+                break;
+            case Directions.S.dir:
+                    if (flip(player, k, s)) flipColor(k+1, s, player, x+1, y);
+                    possibleMove[0] = k;
+                    possibleMove[1] = s;
+                    return possibleMove;
+                break;
+            case Directions.SW.dir:
+                break;
+            case Directions.W.dir:
+                break;
         }
     }
 
-    public int checkBorders(int x, int y, int player) {
+    public int[] checkBorders(int x, int y, int player) {
             for (int k=x-1; k<=x+1; k++) {
                 for (int s=y-1; s<=y+1; s++) {
                     if (board[k][s]!=player && board[k][s]!=EMPTY) {
-                        flipColor(k, s, player, x, y);                        
+                        return flipColor(k, s, player, x, y);                        
                     }
                 }
             }
@@ -97,7 +138,7 @@ public class Reversie {
         for (int i=0; i<board.length; i++) {
             for (int j=0; j<board.length; j++) {
                 if (board[i][j] == player) {
-                    checkBorders(i, j, player);
+                   checkBorders(i, j, player);
                 }
             }
         }
